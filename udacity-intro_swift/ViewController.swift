7e7cd5eee6fd04ad3638ad64f775b33901cc143a
Alexandro <alexandro.gomes@imusica.com.br>
2015-08-19 17:46:27 -0300

    initial commit

---
+++ udacity-intro_swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  udacity-intro_swift
+//
+//  Created by Alexandro on 8/19/15.
+//  Copyright (c) 2015 Alexandro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
